Refactored with real model and fixed noise backprop bug

svg_torch.py
@@ -11,7 +11,6 @@ from envs import pendulum_torch
 
 
 # TODO recall that gradient explode
-# TODO make infer noise
 
 class Policy(nn.Module):
     def __init__(self, obs_space, action_space, h_dim=32):
@@ -26,16 +25,43 @@ class Policy(nn.Module):
         sigma = F.softplus(sigma)
         return mu, sigma
 
+    @torch.no_grad()
+    def sample(self, s):
+        mu, sigma = self(s)
+        eps = torch.randn(mu.shape)
+        a = (mu + sigma * eps)
+        return a, eps
+
+    def rsample(self, s):
+        mu, sigma = self(s)
+        eps = torch.randn(mu.shape)
+        a = (mu + sigma * eps)
+        return a, eps
+
+class RealDynamics:
+    def __init__(self, env):
+        self._f = env._f
+        self.r = env._r
+        self.std = 1
+
+    def f(self, s, a):
+        mu, sigma = self._f(s, a), self.std
+        return mu, sigma
+
 
 def get_grad_norm(parameters):
     return torch.norm(torch.cat([p.grad.flatten() for p in parameters]))
 
 
-def infer_noise(transition, dynamics_fn):
+def infer_noise(transition, dynamics, policy):
     s, a, r, s1, d, eps, eta = transition
-    s1_hat = dynamics_fn(s, a)
-    eta = (s1 - s1_hat)
-    return (eps, eta)
+
+    s1_hat, sigma = dynamics.f(s, a)
+    eta = (s1 - s1_hat) / sigma
+
+    a_hat, sigma = policy(s)
+    eps = (a - a_hat) / sigma
+    return eps.detach(), eta.detach()
 
 
 def unroll(dynamics, policy, traj):
@@ -44,11 +70,13 @@ def unroll(dynamics, policy, traj):
     s = traj[0][0]
     # scale = 0.5 ** 2
     for t, transition in enumerate(traj):
-        action_noise, model_noise = infer_noise(transition, dynamics._f)
+        action_noise, model_noise = infer_noise(transition, dynamics, policy)
         mu, sigma = policy(s)
         a = mu + sigma * action_noise
-        r = dynamics._r(s, a)
-        s1 = dynamics._f(s, a)
+        r = dynamics.r(s, a)
+        s1 = dynamics.f(s, a)
+        mu, sigma = dynamics.f(s, a)
+        s1 = mu + sigma * model_noise
         s = s1
         total_reward += (gamma ** t) * r
         # s = s * scale + (1 - scale) * s.detach()
@@ -86,9 +114,7 @@ def generate_episode(env, policy, gamma=0.99):
     total_reward = 0
     t = 0
     while not d:
-        mu, sigma = policy(s)
-        eps = torch.randn((1,))
-        a = (mu + sigma * eps).detach()
+        a, eps = policy.sample(s)
         s1, r, d, info = env.step(a)
         trajectory.append((s, a, r, s1, d, eps, 0.))
         s = s1
@@ -101,18 +127,18 @@ def main():
     torch.manual_seed(0)
     env = pendulum_torch.Pendulum()
     policy = Policy(3, 1)
+    dynamics = RealDynamics(env)
     dtm = datetime.now().strftime("%d-%H-%M-%S-%f")
     pi_optim = optim.SGD(policy.parameters(), lr=1e-3)
     writer = tb.SummaryWriter(log_dir=f"logs/{dtm}")
     save_every = int(1e3)
     for epoch in range(int(1e4)):
         traj, env_reward = generate_episode(env, policy)
-        reward, grad_norm = train(env, policy, pi_optim, traj)
+        reward, grad_norm = train(dynamics, policy, pi_optim, traj)
         if epoch % save_every == 0:
             torch.save(policy.state_dict(), os.path.join("logs", dtm, "ckp.pb"))
         writer.add_scalar("train/reward", reward, global_step=epoch)
         writer.add_scalar("env/reward", env_reward, global_step=epoch)
-        writer.add_scalar("train/grad_norm", grad_norm, global_step=epoch)
         writer.add_scalar("train/grad_norm", grad_norm, global_step=epoch)
 
 