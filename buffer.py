b5fec2679702b7a96af75d40c747f8996ee1b540
proceduralia <pierluca.doro@gmail.com>
2021-02-25 14:48:43 +0100

    svg (probably) working pytorch version

---
+++ buffer.py
@@ -0,0 +1,128 @@
+import torch
+import numpy as np
+import warnings
+
+
+class Buffer:
+    def __init__(self, d_state, d_action, size):
+        """
+        data buffer that holds transitions
+        Args:
+            d_state: dimensionality of state
+            d_action: dimensionality of action
+            size: maximum number of transitions to be stored (memory allocated at init)
+        """
+        # Dimensions
+        self.size = size
+        self.d_state = d_state
+        self.d_action = d_action
+
+        # Main Attributes
+        self.states = torch.zeros(size, d_state).float()
+        self.actions = torch.zeros(size, d_action).float()
+        self.state_deltas = torch.zeros(size, d_state).float()
+        self.rewards = torch.zeros(size).float()
+
+        # Other attributes
+        self.normalizer = None
+        self.ptr = 0
+        self.is_full = False
+
+    def setup_normalizer(self, normalizer):
+        self.normalizer = normalizer
+
+    def _add(self, buffer, arr):
+        n = arr.size(0)
+        excess = self.ptr + n - self.size  # by how many elements we exceed the size
+        if excess <= 0:  # all elements fit
+            a, b = n, 0
+        else:
+            a, b = n - excess, excess  # we need to split into a + b = n; a at the end and the rest in the beginning
+        buffer[self.ptr:self.ptr + a] = arr[:a]
+        buffer[:b] = arr[a:]
+
+    def add(self, states, actions, next_states, rewards):
+        """
+        add transition(s) to the buffer
+        Args:
+            states: pytorch Tensors of (n_transitions, d_state) shape
+            actions: pytorch Tensors of (n_transitions, d_action) shape
+            next_states: pytorch Tensors of (n_transitions, d_state) shape
+        """
+        states, actions, next_states, rewards = [x.clone().cpu() for x in [states, actions, next_states, rewards]]
+
+        state_deltas = next_states - states
+        n_transitions = states.size(0)
+
+        assert n_transitions <= self.size
+
+        self._add(self.states, states)
+        self._add(self.actions, actions)
+        self._add(self.state_deltas, state_deltas)
+        self._add(self.rewards, rewards)
+
+        if self.ptr + n_transitions > self.size or self.is_full:
+            warnings.warn("Buffer overflow. Rewriting old samples")
+
+        if self.ptr + n_transitions >= self.size:
+            self.is_full = True
+
+        self.ptr = (self.ptr + n_transitions) % self.size
+
+        if self.normalizer is not None:
+            for s, a, ns, r in zip(states, actions, state_deltas, rewards):
+                self.normalizer.add(s, a, ns, r)
+
+    def add_trajectory(self, traj):
+        states = torch.stack([transition[0] for transition in traj])
+        actions = torch.stack([transition[1] for transition in traj])
+        rewards = torch.stack([transition[2] for transition in traj])
+        next_states = torch.stack([transition[3] for transition in traj])
+
+        self.add(states, actions, next_states, rewards)
+
+    def view(self):
+        n = len(self)
+
+        s = self.states[:n]
+        a = self.actions[:n]
+        s_delta = self.state_deltas[:n]
+        ns = s + s_delta
+
+        return s, a, ns, s_delta
+
+    def train_batches(self, batch_size):
+        """
+        return an iterator of batches
+        Args:
+            batch_size: number of samples to be returned
+        Returns:
+            state of size (n_samples, d_state)
+            action of size (n_samples, d_action)
+            next state of size (n_samples, d_state)
+        """
+        num = len(self)
+        indices = np.random.permutation(range(num))
+
+        for i in range(0, num, batch_size):
+            j = min(num, i + batch_size)
+
+            if (j - i) < batch_size and i != 0:
+                # drop the last incomplete batch
+                return
+
+            batch_size = j - i
+            batch_indices = indices[i:j]
+
+            states = self.states[batch_indices]
+            actions = self.actions[batch_indices]
+            state_deltas = self.state_deltas[batch_indices]
+            next_states =  states + state_deltas
+
+            yield states, actions, next_states
+
+    def __len__(self):
+        return self.size if self.is_full else self.ptr
+
+    def __getstate__(self):
+        return self.__dict__.copy()
--- svg_torch.py
+++ svg_torch.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 
 import torch
@@ -6,8 +5,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.tensorboard as tb
+from buffer import Buffer
 
 from envs import pendulum_torch
+
+
+# TODO substitute with proper config object
+from types import SimpleNamespace
+config = SimpleNamespace(use_oracle=False, initial_steps=100,
+                         policy_lr=2e-2, model_lr=1e-3,
+                         train_on_buffer=False)
+
+
+class Trajectory:
+    def __init__(self):
+        self._data = []
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def append(self, transition):
+        self._data.append(transition)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        return iter(self._data)
 
 
 class Policy(nn.Module):
@@ -36,6 +60,7 @@ class Policy(nn.Module):
         a = (mu + sigma * eps)
         return a, eps
 
+
 class RealDynamics:
     def __init__(self, env):
         self._f = env._f
@@ -53,13 +78,13 @@ class LearnedDynamics(nn.Module):
         self.r = env._r
         obs_dim = env.observation_space.shape[0]
         action_dim = env.action_space.shape[0]
-        self.fc = nn.Sequential(*[nn.Linear(obs_dim+action_dim, h_dim), nn.Tanh(), nn.Linear(h_dim, h_dim), nn.Tanh()])
+        self.fc = nn.Sequential(*[nn.Linear(obs_dim+action_dim, h_dim), nn.ELU()])
         self.out = nn.Linear(h_dim, obs_dim)
         self.std = 1
 
     def forward(self, s, a):
-        h = self.fc(torch.cat((s,a), dim=-1))
-        mu = self.out(h)
+        h = self.fc(torch.cat((s, a), dim=-1))
+        mu = self.out(h) + s
         return mu, self.std
 
     def f(self, s, a):
@@ -70,34 +95,30 @@ def get_grad_norm(parameters):
     return torch.norm(torch.cat([p.grad.flatten() for p in parameters]))
 
 
-def infer_noise(transition, dynamics, policy):
+def recreate_transition(transition, dynamics, policy):
     s, a, r, s1, d, eps, eta = transition
 
     s1_hat, sigma = dynamics.f(s, a)
     eta = (s1 - s1_hat) / sigma
+    mu, sigma = dynamics.f(s, a)
+    ns = mu + sigma * eta.detach()
 
     a_hat, sigma = policy(s)
     eps = (a - a_hat) / sigma
-    return eps.detach(), eta.detach()
+    mu, sigma = policy(s)
+    ac = mu + sigma * eps.detach()
+    return ac, ns
 
 
 def unroll(dynamics, policy, traj):
     gamma = 0.99
     total_reward = 0
     s = traj[0][0]
-    # scale = 0.5 ** 2
     for t, transition in enumerate(traj):
-        action_noise, model_noise = infer_noise(transition, dynamics, policy)
-        mu, sigma = policy(s)
-        a = mu + sigma * action_noise
+        a, ns = recreate_transition(transition, dynamics, policy)
         r = dynamics.r(s, a)
-        s1 = dynamics.f(s, a)
-        mu, sigma = dynamics.f(s, a)
-        s1 = mu + sigma * model_noise
-        s = s1
+        s = ns
         total_reward += (gamma ** t) * r
-        # s = s * scale + (1 - scale) * s.detach()
-        # total_reward =  total_reward * scale + (1-scale) * total_reward.detach()
     return total_reward
 
 
@@ -110,24 +131,6 @@ def train(dynamics, policy, pi_optim, traj, opt_steps=1):
         nn.utils.clip_grad_value_(policy.parameters(), 5.)
         pi_optim.step()
     return total_reward, true_norm
-
-
-class Trajectory:
-
-    def __init__(self):
-        self._data = []
-
-    def __getitem__(self, item):
-        return self._data[item]
-
-    def append(self, transition):
-        self._data.append(transition)
-
-    def __len__(self):
-        return len(self._data)
-
-    def __iter__(self):
-        return iter(self._data)
 
 
 def generate_episode(env, policy, gamma=0.99):
@@ -161,29 +164,54 @@ def train_model_on_traj(dynamics, traj, model_optim, batch_size=64):
     return loss
 
 
+def train_model_on_buffer(dynamics, buffer, model_optim, batch_size=64, n_batches=16):
+    buffer_it = buffer.train_batches(batch_size)
+
+    for i in range(n_batches):
+        try:
+            s, a, ns = next(buffer_it)
+        except StopIteration:
+            break
+        model_optim.zero_grad()
+        mu, _ = dynamics(s, a)
+        loss = nn.functional.mse_loss(mu, ns)
+        loss.backward()
+        model_optim.step()
+    return loss
+
+
 def main():
     torch.manual_seed(0)
     env = pendulum_torch.Pendulum()
     policy = Policy(3, 1)
-    #dynamics = RealDynamics(env)
-    dynamics = LearnedDynamics(env)
+    if config.use_oracle:
+        dynamics = RealDynamics(env)
+    else:
+        if config.train_on_buffer:
+            buffer = Buffer(3, 1, 2048)
+        dynamics = LearnedDynamics(env)
 
-    pi_optim = optim.SGD(policy.parameters(), lr=1e-3)
-    model_optim = optim.SGD(dynamics.parameters(), lr=1e-4)
+    pi_optim = optim.SGD(policy.parameters(), lr=config.policy_lr)
+    model_optim = None if config.use_oracle else optim.SGD(dynamics.parameters(), lr=config.model_lr)
 
     dtm = datetime.now().strftime("%d-%H-%M-%S-%f")
     writer = tb.SummaryWriter(log_dir=f"logs/{dtm}")
-    save_every = int(1e3)
     for epoch in range(int(1e4)):
         traj, env_reward = generate_episode(env, policy)
-        model_loss = train_model_on_traj(dynamics, traj, model_optim)
-        reward, grad_norm = train(dynamics, policy, pi_optim, traj)
-        if epoch % save_every == 0:
-            torch.save(policy.state_dict(), os.path.join("logs", dtm, "ckp.pb"))
-        writer.add_scalar("train/reward", reward, global_step=epoch)
-        writer.add_scalar("train/model_loss", model_loss, global_step=epoch)
-        writer.add_scalar("env/reward", env_reward, global_step=epoch)
-        writer.add_scalar("train/grad_norm", grad_norm, global_step=epoch)
+        writer.add_scalar("env/return", env_reward, global_step=epoch)
+        writer.add_scalar("env/action", float(traj[0][1]), global_step=epoch)
+        if not config.use_oracle:
+            if config.train_on_buffer:
+                buffer.add_trajectory(traj)
+                model_loss = train_model_on_buffer(dynamics, buffer, model_optim)
+            else:
+                model_loss = train_model_on_traj(dynamics, traj, model_optim)
+            writer.add_scalar("train/model_loss", model_loss, global_step=epoch)
+
+        if epoch > config.initial_steps or config.use_oracle:
+            ret, grad_norm = train(dynamics, policy, pi_optim, traj)
+            writer.add_scalar("train/return", ret, global_step=epoch)
+            writer.add_scalar("train/grad_norm", grad_norm, global_step=epoch)
 
 
 if __name__ == '__main__':
