9313ab37739ad07ed6febe092f2ef199750aec3a
d3sm0 <simone.totaro@gmail.com>
2021-03-07 19:36:51 +0100

    [CLUSTER] test

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ NUM_PROCS = 1
 sweep_yaml = "sweep.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
-
+seed = 0
 use_cuda = False
 
 mila_tools.register(locals())
