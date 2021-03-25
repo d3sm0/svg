c39ccdbc0d750dbe5b33d3d6b262ae98f21184aa
d3sm0 <simone.totaro@gmail.com>
2021-03-25 03:13:27 +0100

    [CLUSTER] elu

--- models.py
+++ models.py
@@ -16,16 +16,30 @@ class Policy(nn.Module):
         super().__init__()
         self.obs_dim = obs_dim
         self.action_dim = action_dim
-        self.fc = nn.Sequential(*[nn.Linear(obs_dim, h_dim), nn.Tanh(), nn.Linear(h_dim, h_dim), nn.Tanh()])
+        self.fc = nn.Sequential(nn.Linear(obs_dim, h_dim),
+                                nn.Tanh(),
+                                nn.Linear(h_dim, h_dim),
+                                nn.Tanh()
+                                )
         self.out = nn.Linear(h_dim, 2 * action_dim)
+
+        self._value = nn.Sequential(nn.Linear(obs_dim, h_dim),
+                                    nn.Tanh(),
+                                    nn.Linear(h_dim, h_dim),
+                                    nn.Tanh(),
+                                    nn.Linear(h_dim, 1),
+                                    )
         self.apply(weights_init)
 
     def forward(self, s):
         h = self.fc(s)
         out = self.out(h)
         mu, sigma = torch.split(out, self.action_dim, -1)
-        sigma = F.softplus(sigma)
+        sigma = F.softplus(sigma).clamp_max(1.)
         return mu, sigma + 1e-3
+
+    def value(self, x):
+        return self._value(x)
 
     def sample(self, s):
         mu, sigma = self(s)
@@ -63,23 +77,26 @@ class Dynamics(nn.Module):
         obs_dim = env.observation_space.shape[0]
         action_dim = env.action_space.shape[0]
 
-        if learn_reward:
-            self.reward = Reward(obs_dim + action_dim, h_dim)
-        else:
-            assert hasattr(env.unwrapped, "reward"), "Env must expose reward()"
-            self.reward = env.unwrapped.reward
+        self.reward = env.unwrapped.reward
 
         self.fc = nn.Sequential(
-            *[nn.Linear(obs_dim + action_dim, h_dim), nn.Tanh(), nn.Linear(h_dim, h_dim), nn.Tanh()])
-        self.out = nn.Linear(h_dim, obs_dim)
+            nn.Linear(obs_dim + action_dim, h_dim),
+            nn.ELU(),
+            nn.Linear(h_dim, h_dim),
+            nn.ELU()
+        )
+        self.out = nn.Linear(h_dim, 2 * obs_dim)
         self.std = std
 
         self.apply(weights_init)
 
     def forward(self, s, a):
         h = self.fc(torch.cat((s, a), dim=-1))
-        mu = self.out(h) + s
-        return mu, self.std
+        dist = self.out(h)
+        mu, sigma = torch.split(dist, s.shape[-1], -1)
+        sigma = F.softplus(sigma) + 1e-3
+        return nn.Sigmoid()(sigma), torch.clamp(sigma, -1., 1.)
 
-    def f(self, s, a):
-        return self(s, a)
+    def __call__(self, s, a):
+        s_tp1, sigma = self.forward(s, a)
+        return s_tp1, sigma
