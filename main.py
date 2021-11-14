957387ad696825418da8d5b19111bab2b8cdba39
d3sm0 <simone.totaro@gmail.com>
2021-11-14 16:48:29 -0500

    [CLUSTER] sweep

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import itertools
 
+import experiment_buddy as buddy
 import torch
 import torch.optim as optim
-import config
-import experiment_buddy as buddy
+from gym.envs.classic_control import PendulumEnv
 
+import config
+import svg
+from buffer import Trajectory, Transition
+from envs.pendulum import Pendulum
 from eval_policy import eval_policy
 from models import Agent
-from envs.pendulum import Pendulum
-import svg
-import utils
 
-
-
-from buffer import Trajectory, Transition
-from gym.envs.classic_control import PendulumEnv
 
 # this should go in buddy
 def scalars_to_tb(writer, scalars, global_step):
     for k, v in scalars.items():
         writer.add_scalar(k, v, global_step)
+
 
 def gather_trajectory(env, agent, gamma=0.99):
     state = env.reset(0)
@@ -27,49 +25,48 @@ def gather_trajectory(env, agent, gamma=0.99):
     total_return = 0
     t = 0
     while not state.done:
-        action,eps = agent.get_action(state.obs)
+        action, eps = agent.get_action(state.obs)
         next_state = env.step(state, action)
         trajectory.append(Transition(state.obs, action, next_state.reward, next_state.obs, next_state.done, eps))
         # replay_buffer.append(Transition(state.obs, action, next_state.reward, next_state.obs, next_state.done, eps))
         state = next_state
-        total_return += state.reward * gamma**t
-        t+=1
+        total_return += state.reward * gamma ** t
+        t += 1
     return trajectory, total_return
 
 
 def main():
     torch.manual_seed(config.seed)
     buddy.register_defaults(config.__dict__)
-    tb = buddy.deploy(disabled=True)
-    env = Pendulum() # agent follows brax convention
+    tb = buddy.deploy(proc_num=config.proc_num,host=config.host,sweep_yaml=config.sweep_yaml)
+    env = Pendulum()  # agent follows brax convention
     agent = Agent(env.observation_size, env.action_size, h_dim=config.h_dim)
     actor_optim = optim.Adam(agent.actor.parameters(), lr=config.policy_lr)
-    critic_optim= optim.Adam(agent.critic.parameters(), lr=config.policy_lr)
-    run(env, agent, actor_optim,critic_optim,tb)
+    critic_optim = optim.Adam(agent.critic.parameters(), lr=config.critic_lr)
+    run(env, agent, actor_optim, critic_optim, tb)
 
 
-def run(env, agent, actor_optim ,critic_optim, tb):
+def run(env, agent, actor_optim, critic_optim, tb):
     n_samples = 0
 
     for global_step in itertools.count():
         if n_samples >= config.max_steps:
             break
-        #  if global_step % config.save_every == 0:
-       #      print(f"Saved at {global_step}. Progress:{n_samples / config.max_steps:.2f}")
-       #      tb.add_object("model", agent, global_step)
         trajectory, env_return = gather_trajectory(env, agent)
 
-        critic_info = svg.critic(trajectory, agent, critic_optim,batch_size=config.batch_size)
+        critic_info = svg.critic(trajectory, agent, critic_optim, batch_size=config.batch_size)
         # ascend the gradient
-        actor_info= svg.actor(trajectory, agent, env, actor_optim,batch_size=config.batch_size)
-        tb.add_scalar("train/return",env_return,n_samples)
-        scalars_to_tb(tb,{**actor_info,**critic_info},n_samples)
+        actor_info = svg.actor(trajectory, agent, env, actor_optim, batch_size=config.batch_size)
+        tb.add_scalar("train/return", env_return, n_samples)
+        scalars_to_tb(tb, {**actor_info, **critic_info}, n_samples)
         n_samples += 200
-        print(global_step, env_return)
 
-        if global_step % config.save_every == 0 and global_step > 0:
+        if global_step % config.save_every == 0 and global_step > 0 and config.should_render:
             info = eval_policy(PendulumEnv(), agent, log_dir=tb.objects_path)
-            print(info)
+        if global_step % config.save_every == 0:
+            print(f"Saved at {global_step}. Progress:{n_samples / config.max_steps:.2f}")
+            tb.add_object("agent", agent, global_step)
+
 
 if __name__ == "__main__":
     main()
--- svg.py
+++ svg.py
@@ -8,8 +8,8 @@ import utils
 def recreate_transition(state, transition, dynamics, policy):
     # this function makes the gradient along the a trajectory equivalent regardless the parameters of the function
     mu, std = policy(state)
-    eps = (transition.action - mu) /std
-    action = mu+ std* eps.detach()
+    eps = (transition.action - mu) / std
+    action = mu + std * eps.detach()
 
     s1_hat = dynamics(state, action)
     eta = (transition.next_state - s1_hat)
@@ -18,42 +18,42 @@ def recreate_transition(state, transition, dynamics, policy):
     return action, next_state
 
 
-def verify_transition(transition, state,action,reward,next_state):
-    assert torch.allclose(transition.state , state)
-    assert torch.allclose(transition.next_state , next_state)
-    assert torch.allclose(transition.action , action)
-    assert torch.allclose(transition.reward , reward)
+def verify_transition(transition, state, action, reward, next_state):
+    assert torch.allclose(transition.state, state)
+    assert torch.allclose(transition.next_state, next_state)
+    assert torch.allclose(transition.action, action)
+    assert torch.allclose(transition.reward, reward)
 
-def unroll(trajectory, agent, model,  gamma):
+
+def unroll(trajectory, agent, model, gamma):
     total_return = torch.tensor(0.)
     state = trajectory[0].state
     for t, transition in enumerate(trajectory):
         # Remember as the policy drift  the action drift and thus the visited state
-        mu, std=  agent.forward(state)
+        mu, std = agent.forward(state)
         action = mu + std * transition.noise
         # action = action + (transition.action - action)
-        next_state= model.dynamics(state,action) + (transition.next_state- model.dynamics(state,action))
+        next_state = model.dynamics(state, action) + (transition.next_state - model.dynamics(state, action))
         reward = model.reward(state, action)
-        verify_transition(transition,state,action,reward,next_state)
+        verify_transition(transition, state, action, reward, next_state)
         # action, next_state = recreate_transition(transition, policy)
         # Assume we get the reward upon transition to s1 and not at s1
         total_return += (gamma ** t) * reward
         state = next_state
     # we do not change the value function here, but we need to backprop throuhg
-    total_return += (1-transition.done) * agent.value(state).squeeze() * gamma ** config.horizon
+    total_return += (1 - transition.done) * agent.value(state).squeeze() * gamma ** config.horizon
     return total_return
 
 
-
-def one_step(transition, agent,model,gamma):
-    mu ,std = agent.forward(transition.state)
+def one_step(transition, agent, model, gamma):
+    mu, std = agent.forward(transition.state)
     action = mu + std * transition.noise
-    r = vmap(model.reward)(transition.state,action)
-    next_state = vmap(model.dynamics)(transition.state,action)
+    r = vmap(model.reward)(transition.state, action)
+    next_state = vmap(model.dynamics)(transition.state, action)
     # TODO missing IW if policy drifts off policy
-    value = r + gamma *(1-transition.done) * agent.value(next_state).squeeze()
+    value = r + gamma * (1 - transition.done) * agent.value(next_state).squeeze()
     return value, {
-        "std":torch.linalg.norm(std,1).mean()
+        "std": torch.linalg.norm(std, 1).mean()
     }
 
 
@@ -62,17 +62,17 @@ def actor(replay_buffer, agent, model, pi_optim, batch_size=32, gamma=0.99):
     n_samples = 0
     pi_optim.zero_grad()
     for transition in replay_buffer.sample(batch_size=batch_size):
-        value,metrics = one_step(transition,agent,model,gamma=gamma)
+        value, metrics = one_step(transition, agent, model, gamma=gamma)
         (-value).mean().backward()
         total_loss += value.mean().detach()
-        n_samples +=1
+        n_samples += 1
     grad_norm = utils.get_grad_norm(agent.actor.parameters())
     pi_optim.step()
     pi_optim.zero_grad()
     return {
-        "actor/loss":total_loss.detach()/n_samples,
+        "actor/loss": total_loss.detach() / n_samples,
         "actor/grad_norm": grad_norm.detach(),
-        "actor/std":metrics.get("std")
+        "actor/std": metrics.get("std")
     }
 
 
@@ -81,18 +81,17 @@ def critic(repay_buffer, agent, pi_optim, batch_size=32, gamma=0.99):
     total_loss = torch.tensor(0.)
     n_samples = 0
     agent.zero_grad()
-    # assert torch.allclose(grad_norm, torch.zeros_like(grad_norm))
-    for (s, a, r, s1,done,noise) in repay_buffer.sample(batch_size):
+    for (s, a, r, s1, done, noise) in repay_buffer.sample(batch_size):
         loss = td_loss(agent, s, r, s1, done, gamma).mean()
         loss.mean().backward()
         total_loss += loss
-        n_samples +=1
+        n_samples += 1
     pi_optim.step()
     grad_norm = utils.get_grad_norm(agent.critic.parameters())
 
-    total_loss = total_loss/n_samples
+    total_loss = total_loss / n_samples
     return {
-        "critic/td":total_loss.detach(),
+        "critic/td": total_loss.detach(),
         "critic/grad_norm": grad_norm.detach(),
     }
 
