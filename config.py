ecb93e92bdcc545378d4a6e1b8784610518dd6f3
d3sm0 <simone.totaro@gmail.com>
2021-11-14 16:46:26 -0500

    [CLUSTER] sweep

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ DEBUG = True
 
 max_steps = int(1e6)
 policy_lr = 1e-3
+critic_lr = 1e-3
 horizon = 200 if not DEBUG else 50
 buffer_size = int(1e3)
 gamma = 0.99
@@ -10,5 +11,10 @@ train_horizon = 5
 grad_clip = 5.
 batch_size = 32
 regularizer = 1e-4
-seed= 33
-h_dim =32
+seed = 33
+h_dim = 32
+should_render = False
+proc_num = 5
+host = "mila"
+
+sweep_yaml ="sweep.yaml"
