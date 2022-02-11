cd0507b06a4a0b88a1ff53b6bcf6363009ecc65d
d3sm0 <simone.totaro@gmail.com>
2022-02-11 17:14:33 -0500

    Cleanup svg 0

--- agents.py
+++ agents.py
@@ -4,41 +4,6 @@ import torch.optim as optim
 
 import config
 import utils
-from svg import unroll_trajectory, one_step
-
-
-class SVG:
-    horizon: int = 2
-
-    def __init__(self, agent, horizon: int = 2):
-        self.agent = agent
-        self.horizon = horizon
-
-    def get_value_gradient(self, trajectory, dynamics, gamma):
-        assert len(trajectory) == self.horizon
-        total_return, metrics = unroll_trajectory(trajectory, self.agent, dynamics, gamma)
-        return total_return, metrics
-
-    def get_value_loss(self, batch, gamma):
-        v_t = self.critic(batch.next_state)
-        v_tm1 = self.critic(batch.state)
-        discount_t = (1 - batch.done) * gamma
-        loss = rlego.td_learning(v_tm1, batch.reward, discount_t, v_t, stop_grad=True)
-        return 0.5 * (loss ** 2)
-
-    def update_target(self, tau):
-        return
-
-
-class SVGOne(SVG):
-    def __init__(self, agent):
-        super(SVGOne, self).__init__(agent, horizon=1)
-
-    def get_value_gradient(self, transitions, dynamics, gamma):
-        transition, metrics = one_step(transitions, self.agent, dynamics)
-        one_step_return = transition.reward + gamma * (1 - transition.done) * self.agent.value(
-            transition.next_state).squeeze()
-        return one_step_return, metrics
 
 
 class SVGZero:
@@ -63,8 +28,9 @@ class SVGZero:
         r_t = batch.reward
         vtrace_target = rlego.vtrace_td_error_and_advantage(v_tm1.detach(), v_t, r_t, discount_t,
                                                             rho_tm1=torch.ones_like(discount_t))
-        q_loss = (vtrace_target.q_estimate - q_tm1) * (1-config.gamma) + utils.l2_norm(self.model.critic.named_parameters())
-        td_loss = (vtrace_target.target_tm1 - v_tm1) * (1-config.gamma)
+        q_loss = (vtrace_target.q_estimate - q_tm1) * (1 - config.gamma) + utils.l2_norm(
+            self.model.critic.named_parameters())
+        td_loss = (vtrace_target.target_tm1 - v_tm1) * (1 - config.gamma)
         loss = 0.5 * q_loss.pow(2) + 0.5 * td_loss.pow(2)
         return loss.mean(), {"critic/td": td_loss.mean().detach(),
                              "critic/q_loss": q_loss.mean().detach()
@@ -76,8 +42,8 @@ class SVGZero:
         reg = utils.l2_norm(self.model.actor.named_parameters())
         value = (1. - batch.done) * self.model.critic(batch.state, new_actions).squeeze(-1)
         return (-value).mean(), {"actor/value": value.mean().detach(), "actor/loc": pi.mean.mean(),
-                                       "actor/reg": reg.mean(),
-                                       "actor/scale": pi.variance.mean()}
+                                 "actor/reg": reg.mean(),
+                                 "actor/scale": pi.variance.mean()}
 
     def optimize_critic(self, replay_buffer, epochs=1):
         value_info = optimize_from_buffer(self.model.critic, self._optimize_critic, self.critic_optim,
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import itertools
 
+import brax.envs
+import brax.envs.to_torch
 import experiment_buddy as buddy
 import rlego
 import torch
 
 import agents
 import config
-import envs.lqg
-from envs.utils import GymWrapper
 from models import ActorValue
 
 
@@ -17,11 +17,10 @@ def gather_trajectory(env, agent):
     trajectory = rlego.Trajectory()
     while True:
         pi = agent.model.actor(state)
-        action = pi.rsample()
-        eps = (action - pi.loc) / pi.scale
+        action = pi.sample()
         assert torch.linalg.norm(action) < 1e3
         next_state, reward, done, info = env.step(action)
-        trajectory.append(rlego.Transition(state, action, reward, next_state, done, eps))
+        trajectory.append(rlego.Transition(state, action, reward, next_state, done, torch.cat([pi.loc, pi.scale])))
         state = next_state
         if done:
             break
@@ -38,9 +37,9 @@ def main():
         extra_modules=["python/3.7", "cuda/11.1/cudnn/8.0"],
     )
 
-    env = GymWrapper(envs.lqg.Lqg())
+    env = brax.envs.create_gym_env(config.env_id, backend="cpu")
+    env = brax.envs.to_torch.JaxToTorchWrapper(env)
     model = ActorValue(env.observation_space.shape[0], env.action_space.shape[0], h_dim=config.h_dim).to(config.device)
-    writer.watch(model, log="all", log_freq=10)
     agent = agents.SVGZero(model)
     run(env, agent, writer)
 
@@ -55,7 +54,7 @@ def run(env, agent, writer):
 
         critic_info = agent.optimize_critic(trajectory,
                                             epochs=config.critic_epochs)
-        # ascend the gradient on-policy
+
         actor_info = agent.optimize_actor(trajectory,
                                           epochs=config.actor_epochs)
         if global_step % config.update_target_every == 0:
@@ -65,30 +64,6 @@ def run(env, agent, writer):
             **actor_info,
             **critic_info
         }, global_step=global_step)
-
-
-        # w = agent.model.critic.q[0].weight.T
-        # w_mu = agent.model.actor.pi.linear[0].weight.T[:, :1]
-
-        # if not torch.isfinite(actor_info.get("actor/grad_norm")) or not torch.isfinite(critic_info.get("critic/grad_norm")):
-        #     print("Found nan in loss", critic_info, actor_info)
-        #     break
-        # rolling_performance.append(env_info.get("train/return"))
-        # avg_return = torch.tensor(rolling_performance)
-        # env_info.update({"train/avg_return": avg_return.mean(), "train/avg_std": avg_return.std()})
-        # writer.add_scalars({**actor_info, **critic_info, **env_info}, n_samples)
-
-        # n_samples += env_info.get("train/duration")
-
-
-#         if global_step % config.save_every == 0 and global_step > 0 and config.should_render:
-#             render_policy(env, agent)
-#             # trajectory, env_return = gather_trajectory(env, agent, replay_buffer)
-#
-#         if global_step % config.save_every == 0:
-#             print(f"Saved at {global_step}. Progress:{n_samples / config.max_steps:.2f}")
-#             torch.save(agent, "model.pt")
-#
 
 
 if __name__ == "__main__":
